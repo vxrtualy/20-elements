46c95345377845760f13801620dce2d407143c2b
vxrtualy <95960099+vxrtualy@users.noreply.github.com>
2022-04-21 17:30:38 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,161 @@
+score=0
+e1="hydrogen"
+ans=input("1.   What is the first element of the periodic table? ")
+if not e1 in ans:
+    print("Incorrect: the first element of the periodic table is hydrogen.")
+if e1 == ans:
+    print("Correct.")
+    score+=1
+
+e2="helium"
+ans=input("2.   What is the second element of the periodic table? ")
+if not e2 in ans:
+    print("Incorrect, the second element of the periodic table is helium.")
+if e2 == ans:
+    print("Correct.")
+    score+=1
+
+e3="lithium"
+ans=input("3.   What is the third element of the periodic table? ")
+if not e3 in ans:
+    print("Incorrect, the third element of the periodic table is lithium.")
+if e3 == ans:
+    print("Correct.")
+    score+=1
+
+e4="beryllium"
+ans=input("4.   What is the fourth element of the periodic table? ")
+if not e4 in ans:
+    print("Incorrect, the fourth element of the periodic table is beryllium.")
+if e4 == ans:
+    print("Correct.")
+    score+=1
+
+e5="boron"
+ans=input("5.   What is the fifth element of the periodic table? ")
+if not e5 in ans:
+    print("Incorrect, the fifth element of the periodic table is boron.")
+if e5 == ans:
+    print("Correct.")
+    score+=1
+
+e6="carbon"
+ans=input("6.   What is the sixth element of the periodic table? ")
+if not e6 in ans:
+    print("Incorrect, the fourth element of the periodic table is carbon.")
+if e6 == ans:
+    print("Correct.")
+    score+=1
+
+e7="nitrogen"
+ans=input("7.   What is the seventh element of the periodic table? ")
+if not e7 in ans:
+    print("Incorrect, the fourth seventh of the periodic table is nitrogen.")
+if e7 == ans:
+    print("Correct.")
+    score+=1
+
+e8="oxygen"
+ans=input("8.   What is the eighth element of the periodic table? ")
+if not e8 in ans:
+    print("Incorrect, the eighth element of the periodic table is oxygen.")
+if e8 == ans:
+    print("Correct.")
+    score+=1
+
+e9="fluorine"
+ans=input("9.   What is the ninth element of the periodic table? ")
+if not e9 in ans:
+    print("Incorrect, the ninth element of the periodic table is fluorine.")
+if e9 == ans:
+    print("Correct.")
+    score+=1
+    
+e10="neon"
+ans=input("10.   What is the tenth element of the periodic table? ")
+if not e10 in ans:
+    print("Incorrect, the tenth element of the periodic table is neon.")
+if e10 == ans:
+    print("Correct.")
+    score+=1
+
+e11="neon"
+ans=input("11.   What is the eleventh element of the periodic table? ")
+if not e11 in ans:
+    print("Incorrect, the eleventh element of the periodic table is neon.")
+if e11 == ans:
+    print("Correct.")
+    score+=1
+
+e12="sodium"
+ans=input("12.   What is the twelfth element of the periodic table? ")
+if not e12 in ans:
+    print("Incorrect, the twelfth element of the periodic table is sodium.")
+if e12 == ans:
+    print("Correct.")
+    score+=1
+
+e13="aluminium"
+ans=input("13.   What is the thirteenth element of the periodic table? ")
+if not e13 in ans:
+    print("Incorrect, the thirteenth element of the periodic table is aluminium.")
+if e13 == ans:
+    print("Correct.")
+    score+=1
+
+e14="silicon"
+ans=input("14.   What is the fourteenth element of the periodic table? ")
+if not e14 in ans:
+    print("Incorrect, the fourteenth element of the periodic table is silicon.")
+if e14 == ans:
+    print("Correct.")
+    score +=1
+
+e15="phosphorus"
+ans=input("15.   What is the fifteenth element of the periodic table? ")
+if not e15 in ans:
+    print("Incorrect, the fifteenth element of the periodic table is phosphorus.")
+if e15 == ans:
+    print("Correct.")
+    score+=1
+
+e16="sulfur"
+ans=input("16.   What is the sixteenth element of the periodic table? ")
+if not e16 in ans:
+    print("Incorrect, the sixteenth element of the periodic table is sulfur.")
+if e16 == ans:
+    print("Correct.")
+    score+=1
+
+e17="chlorine"
+ans=input("17.   What is the seventeenth element of the periodic table? ")
+if not e17 in ans:
+    print("Incorrect, the seventeenth element of the periodic table is chlorine.")
+if e17 == ans:
+    print("Correct.")
+    score+=1
+
+e18="argon"
+ans=input("18.   What is the eighteenth element of the periodic table? ")
+if not e18 in ans:
+    print("Incorrect, the eighteenth element of the periodic table is argon.")
+if e18 == ans:
+    print("Correct.")
+    score+=1
+
+e19="potassium"
+ans=input("19.   What is the ninteenth element of the periodic table? ")
+if not e19 in ans:
+    print("Incorrect, the ninteenth element of the periodic table is potassium.")
+if e19 == ans:
+    print("Correct.")
+    score+=1
+
+e20="calcium"
+ans=input("20.   What is the twentieth element of the periodic table? ")
+if not e20 in ans:
+    print("Incorrect, the twentieth element of the periodic table is calcium.")
+if e20 == ans:
+    print("Correct.")
+    score+=1
+print("Nice job, you got", score, "/ 20!")
